7440fbe2452e270c6a95c996719cb9af9ac2180a
himynameisdave <d@velunny.com>
2019-09-09 21:30:10 -0700

    ✨ Rules filenames

--- rules/filenames/off.js
+++ rules/filenames/off.js
@@ -1,11 +1,13 @@
 module.exports = {
-  plugins: ["filenames"],
+  plugins: [
+    'filenames',
+  ],
   rules: {
     // Enforce dash-cased filenames
-    "filenames/match-regex": 0,
+    'filenames/match-regex': 0,
     // Match the file name against the default exported value in the module
-    "filenames/match-exported": 0,
+    'filenames/match-exported': 0,
     // Don't allow index.js files
-    "filenames/no-index": 0
-  }
+    'filenames/no-index': 0,
+  },
 };
