9fcf4e07c22d77deb06bc9cbb12509c03c71e8b0
himynameisdave <dave@fossa.com>
2020-11-15 16:08:01 -0800

    👨‍👨‍👧‍👦 Add all config for find-rules

---
+++ configurations/all.js
@@ -0,0 +1,39 @@
+/**
+ * WARNING!
+ *
+ * This config isn't really meant to be consumed directly.
+ * It only exists for `eslint-find-rules`, so that unadded/new rules can be identified.
+ */
+module.exports = {
+  plugins: [
+    'import',
+    'jest',
+    'jsx-a11y',
+    'node',
+    'promise',
+    'react',
+    'react-hooks',
+    'svelte3',
+    '@typescript-eslint',
+    'unicorn',
+  ],
+  'extends': [
+    '../rules/eslint/best-practices/on.js',
+    '../rules/eslint/errors/on.js',
+    '../rules/eslint/es6/on.js',
+    '../rules/eslint/node/on.js',
+    '../rules/eslint/strict/on.js',
+    '../rules/eslint/style/on.js',
+    '../rules/eslint/variables/on.js',
+    '../rules/import/on.js',
+    '../rules/jest/on.js',
+    '../rules/jsx-a11y/on.js',
+    '../rules/node/on.js',
+    '../rules/promise/on.js',
+    '../rules/react/on.js',
+    '../rules/react-hooks/on.js',
+    '../rules/svelte/on.js',
+    '../rules/typescript/on.js',
+    '../rules/unicorn/on.js',
+  ],
+};
